28006f7623df2204e6ac7512d608e5eba684cd01
MateoElias <hadadmateo1@gmail.com>
2021-08-31 18:47:56 -0400

    fix

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ My current Discord Bot Structure
 ### Packages
 **[discord.js](https://www.npmjs.com/package/discord.js)** | *1.11 Mb*
 
+[ms](https://www.npmjs.com/package/ms) | *6.72 Kb*
+
 [fastest-levenshtein](https://www.npmjs.com/package/fastest-levenshtein) | *11 Kb*
 
 [chalk](https://www.npmjs.com/package/chalk) | *35 Kb*
--- commands/Main/announce.js
+++ commands/Main/announce.js
@@ -8,32 +8,7 @@ module.exports = {
     description: "Announcement Command",
     run: async (client, message, args) => {
 
-        const ping = args[0].toLowerCase()
-
-        const chnl = message.guild.channels.cache.find((ch) => ch.name === "statements");
-
-        const sending1 = new MessageEmbed()
-        .setTitle("Sending . . .")
-        .setColor('ffbb17')
-
-        var sent = new MessageEmbed()
-        .setTitle("Success!")
-        .setDescription('Your announcement has been published successfully.')
-        .setColor('4cb913')
-    
-        async function Sending(){
-            var sending = await message.channel.send(sending1)
-            sending.edit(sent)
-            setTimeout(async () => {await sending.delete();}, 6000);
-            message.delete();
-        }
-
-        const announcement = new MessageEmbed()
         
-
-        if(!chnl) return message.channel.send("I was unable to find a channel to send the message.")
-
-        const MSG = message.content.split(" ").slice(2).join(" ")
 
     }
 }
