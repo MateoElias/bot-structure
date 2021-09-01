ca57fe776c884c4ec70fa973589a6bdc0d4bfb25
MateoElias <hadadmateo1@gmail.com>
2021-09-01 14:18:11 -0400

    Fixed howgay lmao

--- commands/Fun/howgay.js
+++ commands/Fun/howgay.js
@@ -6,20 +6,18 @@ module.exports = {
   description: "Shows how gay you are lmao",
   run: (client, message, args) => {
     
-    var name = message.mentions.users.first()
+    var name = message.mentions.users.first() || args.join(" ")
     var percent = Math.floor(Math.random() * 100)
-    
-    if(!name) {
     var resp = new MessageEmbed()
         .setTitle("Homosexuality Rate")
         .setColor('RANDOM')
-        .setDescription(`🏳️‍🌈 You are ${percent}% gay 🏳️‍🌈`)
+    
+    if(!name) {
+      resp.setDescription(`🏳️‍🌈 You are ${percent}% gay 🏳️‍🌈`)
      message.channel.send({embeds: [resp]})
     } else { 
       resp = new MessageEmbed()
-        .setTitle("Homosexuality Rate")
-        .setColor('RANDOM')
-        .setDescription(`🏳️‍🌈 ${name} is ${percent}% gay 🏳️‍🌈`)
+        resp.setDescription(`🏳️‍🌈 ${name} is ${percent}% gay 🏳️‍🌈`)
       message.channel.send({embeds: [resp]})
     }
   }
